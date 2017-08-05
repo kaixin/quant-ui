9d334b6cc3dd541a696546bf03b044cc316a90d4
Jiangxia Nan <jiangxia.nan@oracle.com>
2017-08-05 11:12:00 +0800

    add new empty page AIadvisor

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import AboutUs from '@/components/AboutUs'
+import AIadvisor from '@/components/Aiadvisor'
 
 Vue.use(Router)
 
@@ -16,6 +17,14 @@ export default new Router({
     	path: '/aboutUs',
     	name: 'AboutUs',
     	component: AboutUs
+    },
+    {
+      path: '/AIadvisor',
+      name: 'AIadvisor',
+      component: AIadvisor
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return {x:0, y:0};
+  }
 })
