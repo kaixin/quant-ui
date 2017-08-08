7d7261aaba3964bcd0ab2d4ca80483d6b4f1d3ae
Jiangxia Nan <jiangxia.nan@oracle.com>
2017-08-08 21:08:05 +0800

    add AI advisor page

--- src/components/AIadvisor.vue
+++ src/components/AIadvisor.vue
@@ -1,6 +1,24 @@
 <template>
-	<div>
+	<div id="AI-advisor">
 		<nav-bar :navMenuItems="AIadvisorMenu"></nav-bar>
+
+		<div class="main">
+			<el-row type="flex" justify="center">
+
+				<el-col :span="3">
+					<el-menu default-active="AssetsSteward" class="AI-nav-menu">
+						<el-menu-item class="font-21" v-for="item in AIadvisorLeftMenu" :key="item.index" :index="item.index">
+							<router-link v-text="item.title" :to="item.index"></router-link>
+						</el-menu-item>
+					</el-menu>
+				</el-col>
+
+				<el-col :span="15">
+					<router-view></router-view>
+				</el-col>
+
+			</el-row>
+		</div>
 
 		<footer-bar></footer-bar>
 	</div>
@@ -19,10 +37,16 @@ export default {
 		return {
 			AIadvisorMenu: [
 				{title: "首页", link: "/", selected: false},
-				{title: "智能投顾", link: "/AIadvisor", selected: true},
+				{title: "智能投顾", link: "/AIadvisor/assetsSteward", selected: true},
 				{title: "AI社区", link: "", selected: false},
 				{title: "清华量协", link: "", selected: false},
 				{title: "个人账号", link: "", selected: false}
+			],
+			AIadvisorLeftMenu: [
+				{title: 'AI资产管家', index: '/AIadvisor/assetsSteward'},
+				{title: 'AI产品经理', index: '/AIadvisor/productManager'},
+				{title: 'AI策略师', index: '/AIadvisor/strategist'},
+				{title: 'AI私家定制', index: '/AIadvisor/customBuilt'}
 			]
 		}
 	}
@@ -30,5 +54,31 @@ export default {
 </script>
 
 <style lang="scss">
+	#AI-advisor {
+		.main {
+			margin-top: 50px;
+			background-color: white;
+		}
 
+		.AI-nav-menu {
+			background-color: white;
+
+			a {
+				text-decoration: none;
+				color: black;
+
+				&.router-link-active {
+					color: #20a0ff;
+				}
+			}		
+		}
+
+		.font-21 {
+			font-size: 21px;
+		}
+
+		.font-18 {
+			font-size: 18px;
+		}
+	}
 </style>
