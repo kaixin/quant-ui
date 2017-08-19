0b534ca6abf2153a7e9725c557b736d2c86fa528
kaixin <1074861820@qq.com>
2017-08-19 20:41:41 +0800

    Add build pane for AI community - MyStrategy

--- src/components/views/AICommunity/MyStrategy.vue
+++ src/components/views/AICommunity/MyStrategy.vue
@@ -30,20 +30,53 @@
 						</el-col>
 					</el-row>
 					<el-row>
-						<div style="border: 1px solid black; width: 100%; height: 100%; margin: 0 auto;">
+						<div style="border: 1px solid black; width: 100%; height: 600px; margin: 0 auto;">
 
 						</div>
 					</el-row>
 				</el-col>
-				<el-col :span="12" style="border: 1px solid black;" :class="{hidePane: shouldHideBuildPane}">running</el-col>
+				<el-col :span="12" :class="{hidePane: shouldHideBuildPane}" style="height: 100%;">
+					<el-row class="build-toolbar" :class="{hidePane: shouldHideBuildToolbarPane}">
+						<el-col :span="20">
+							<div>
+								<el-date-picker class="quant-date" v-model="startDate" type="date" placeholder="选择日期"></el-date-picker>
+								<span>至</span>
+								<el-date-picker class="quant-date" v-model="endDate" type="date" placeholder="选择日期"></el-date-picker>
+
+								<span>￥</span>
+								<el-input v-model="amountMoney" style="width: 122px;"></el-input>
+								<el-select v-model="frequency" placeholder="请选择" style="width: 84px;">
+									<el-option v-for="fre in freOptions" :key="fre.value" :label="fre.label" :value="fre.value"></el-option>
+								</el-select>
+							</div>
+						</el-col>
+						<el-col :span="2" :offset="1">
+							<el-button type="primary">运行回测</el-button>
+						</el-col>
+					</el-row>
+					<el-row :class="{hidePane: shouldHideBuildChartPane}">
+						<div style="border: 1px solid black; width: 100%; height: 300px; margin: 0 auto;"></div>
+					</el-row>
+					<el-row>
+						<el-row>
+							<span class="log-tab" @click="showLogMsg">日志</span>
+							<span class="log-tab" @click="showErrorMsg">错误</span>
+							<span style="float: right; padding: 10px"><img :src="toggleMsgPaneIcon" @click="toggleMsgPane"></span>
+						</el-row>
+						<el-row>
+							<div class="log-pane" v-text="outputMsg"></div>
+						</el-row>
+					</el-row>
+				</el-col>
 			</el-row>
 		</div>
 
-		<footer-bar></footer-bar>
+		<!-- <footer-bar></footer-bar> -->
 	</div>
 </template>
 
 <script>
+// import Ace from "ace-builds/src/ace"
 import NavBar from "@/components/layout/NavBar"
 import FooterBar from "@/components/layout/FooterBar"
 
@@ -56,9 +89,25 @@ export default {
 		return {
 			goBackIcon: './static/go-back.png',
 			toggleIDEIcon: './static/right-expand.png',
+			toggleMsgPaneIcon: './static/up-expand.png',
 			IDECols: 12,
 			shouldHideIDEPane: false,
 			shouldHideBuildPane: false,
+			shouldHideBuildToolbarPane: false,
+			shouldHideBuildChartPane: false,
+			startDate: "2016-8-19",
+			endDate: "2017-8-19",
+			amountMoney: 100000,
+			frequency: 'day',
+			freOptions: [
+				{value: 'day', label: '每天'},
+				{value: 'minute', label: '分钟'}
+			],
+			activeMsg: 'log',
+			logMsg: "logs message",
+			errorMsg: "error message",
+			outputMsg: '',
+			msgPaneState: 'not-expanded',
 			AIComunityMenu: [
 				{title: "首页", link: "/", selected: false},
 				{title: "智能投顾", link: "/AIadvisor/assetsSteward", selected: false},
@@ -88,6 +137,25 @@ export default {
 				self.shouldHideIDEPane = false;
 				this.shouldHideBuildPane = false;
 			}
+		},
+		showLogMsg: function() {
+			this.outputMsg = this.logMsg;
+		},
+		showErrorMsg: function() {
+			this.outputMsg = this.errorMsg;
+		},
+		toggleMsgPane: function() {
+			if(this.msgPaneState === "not-expanded") {
+				this.shouldHideBuildChartPane = true;
+				this.shouldHideBuildToolbarPane = true;
+				this.msgPaneState = "expanded";
+				this.toggleMsgPaneIcon = "./static/down-expand.png";
+			}else {
+				this.shouldHideBuildChartPane = false;
+				this.shouldHideBuildToolbarPane = false;
+				this.msgPaneState = "not-expanded";
+				this.toggleMsgPaneIcon = "./static/up-expand.png";
+			}
 		}
 	}
 }
@@ -107,7 +175,7 @@ export default {
 				background-color: #EDF0F2;
 
 				.IDE-toolbar {
-					padding: 10px 20px 10px 50px;
+					padding: 15px 20px 15px 50px;
 
 					.toolbar-icon {
 						padding: 5px;
@@ -118,8 +186,35 @@ export default {
 					}
 				}
 
+				.build-toolbar {
+					padding: 10px;
+				}
+
+				.log-tab {
+					display: inline-block;
+					padding: 10px;
+
+					&:hover {
+						background-color: grey;
+					}
+				}
+
+				.log-pane {
+					width: 100%;
+					height: 100%;
+					min-height: 300px;
+					margin: 0 auto;
+					background-color: black;
+					color: white;
+					padding: 10px;
+				}
+
 				.hidePane {
 					display: none;
+				}
+
+				.quant-date {
+					width: 134px;
 				}
 			}
 		}
